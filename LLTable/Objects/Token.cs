cafc86702a684397ea452cbf4e96543cbb76a4b1
Slediks <alzykin14@gmail.com>
2025-05-31 19:38:36 +0300

    Лексер+

--- LLTable/Objects/Token.cs
+++ LLTable/Objects/Token.cs
@@ -1,11 +1,19 @@
+using Newtonsoft.Json;
+
 namespace LLTable.Objects;
 
 public struct Token
 {
+    [JsonProperty("type")]
     public string Type;
+    [JsonProperty("token_name")]
     public string TokenName;
+    [JsonProperty("item")]
     public string TokenValue;
+    [JsonProperty("line")]
     public int Line;
+    [JsonProperty("start_pos")]
     public int StartPos;
+    [JsonProperty("end_pos")]
     public int EndPos;
 }
