cea2b57c466d64b09d67a726dacb43dd6ed59b76
Slediks <alzykin14@gmail.com>
2025-05-31 21:39:24 +0300

    Адаптировал бегунок под работу с лексером

--- LLTable/FileWorkers/Lexer.cs
+++ LLTable/FileWorkers/Lexer.cs
@@ -12,7 +12,7 @@ public class Lexer
         var args = $"{input} tokens.json";
 
         using var pProcess = new System.Diagnostics.Process();
-        pProcess.StartInfo.FileName = @"lexer.exe"; //exe path
+        pProcess.StartInfo.FileName = "lexer.exe"; //exe path
         pProcess.StartInfo.Arguments = args; //argument
         pProcess.StartInfo.UseShellExecute = false;
         pProcess.StartInfo.RedirectStandardOutput = false; //able to read console output
@@ -24,5 +24,18 @@ public class Lexer
         using var r = new StreamReader("tokens.json");
         var json = r.ReadToEnd();
         Tokens = JsonConvert.DeserializeObject<List<Token>>(json) ?? [];
+        ValidateErrors();
+    }
+
+    private void ValidateErrors()
+    {
+        var errors = Tokens.Where(token => token.Type == "error").ToList();
+        if (errors.Count == 0) return;
+
+        var errorMsg = "Синтаксические ошибки:\n" +
+                       String.Join("\n",
+                           errors.Select(error =>
+                               $"{error.TokenName} : {error.TokenValue} ({error.Line}:{error.StartPos})"));
+        throw new Exception(errorMsg);
     }
 }
